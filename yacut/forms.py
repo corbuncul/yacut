f8e2de2c6e077ba56b115131d3a004f0e56c98e1
corbuncul <agafivan@gmail.com>
2024-11-10 18:56:54 +0300

    for review, edit README.md, add constants in modules form.py, models.py

--- yacut/forms.py
+++ yacut/forms.py
@@ -2,6 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, URLField
 from wtforms.validators import DataRequired, Length, Optional
 
+ORIGINAL_MAX_LENGHT = 256
+ORIGINAL_MIN_LENGHT = 1
+CUSTOM_MAX_LENGHT = 16
+CUSTOM_MIN_LENGHT = 1
+
 
 class URLMapForm(FlaskForm):
     """Форма ввода укорачивателя ссылок.
@@ -15,9 +20,16 @@ class URLMapForm(FlaskForm):
     """
     original_link = URLField(
         'Длинная ссылка',
-        validators=[DataRequired(message='Обязательное поле'), Length(1, 256)],
+        validators=[
+            DataRequired(message='Обязательное поле'),
+            Length(ORIGINAL_MIN_LENGHT, ORIGINAL_MAX_LENGHT)
+        ],
     )
     custom_id = StringField(
-        'Ваш вариант короткой ссылки', validators=[Length(1, 16), Optional()]
+        'Ваш вариант короткой ссылки',
+        validators=[
+            Length(CUSTOM_MIN_LENGHT, CUSTOM_MAX_LENGHT),
+            Optional()
+        ]
     )
     submit = SubmitField('Создать')
